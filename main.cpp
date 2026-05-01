7c81ae349915217319a0c0872943fab1bf531b01
humnafatima <humnafatima.khan25@nixorcollege.edu.pk>
2026-05-01 16:21:03 +0500

    raylib setup test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
+#include "raylib.h"
 
 int main() {
-    brain game;
-    game.run();
+    InitWindow(800, 600, "Raylib Test");   // Create a window
+    while (!WindowShouldClose()) {          // Main loop
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow();                          // Close window
+    return 0;
 }
