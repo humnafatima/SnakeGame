d6259b284be6605525895cb7f744a82a0fb09fcd
nehacharyani <nehacharyani123@gmail.com>
2026-05-09 22:48:55 +0500

    two player

--- GameController.h
+++ GameController.h
@@ -1,22 +1,51 @@
 #pragma once
+
 #include "snake.h"
+
 #include "food.h"
+
 #include "obstacle.h"
+
 #include "board.h"
+
 #include <vector>
 
+ 
+
 class GameController {
+
 private:
-    Snake snake;
+
+    Snake snake1;
+
+    Snake snake2;
+
     Food food;
+
     std::vector<Obstacle> obstacles;
+
     Board board;
+
     bool gameOver;
-    int score;
+
+    int score1, score2;
+
+    int winner;  // 1, 2, or 0 for draw
+
+ 
+
+    void reshuffleObstacles();
+
+ 
 
 public:
+
     GameController();
+
     void update();
+
     void draw();
+
     bool isGameOver() const { return gameOver; }
+
 };
