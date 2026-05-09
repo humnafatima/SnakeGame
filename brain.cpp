ed809b2c64c621b6c0262a2227969f4526ef128e
Aleeza999 <samialeeza0@gmail.com>
2026-05-09 21:46:56 +0500

    added brain.cpp changes

--- brain.cpp
+++ brain.cpp
@@ -1,7 +1,28 @@
-//Controls entire game loop
-//Handles: input, output, logic score, game over 
+#include "brain.h"
 
-void input();
-void logic();
-void run();
+Brain::Brain() : current(Direction::RIGHT) {}
 
+void Brain::setDirection(Direction newDir) {
+    if (!isOpposite(newDir, current))
+        current = newDir;
+}
+
+Direction Brain::getDirection() const {
+    return current;
+}
+
+Position Brain::computeNextHead(const Position& head) const {
+    switch (current) {
+        case Direction::UP:    return Position(head.x - 1, head.y);
+        case Direction::DOWN:  return Position(head.x + 1, head.y);
+        case Direction::LEFT:  return Position(head.x, head.y - 1);
+        case Direction::RIGHT: return Position(head.x, head.y + 1);
+    }
+}
+
+bool Brain::isOpposite(Direction a, Direction b) const {
+    return (a == Direction::UP    && b == Direction::DOWN)  ||
+           (a == Direction::DOWN  && b == Direction::UP)    ||
+           (a == Direction::LEFT  && b == Direction::RIGHT) ||
+           (a == Direction::RIGHT && b == Direction::LEFT);
+}
