ad803741d263d96c35357a388fbfa4803fdfba7d
Aleeza999 <samialeeza0@gmail.com>
2026-05-12 00:22:05 +0500

    food files commented

--- food.h
+++ food.h
@@ -1,19 +1,21 @@
 //aleeza
 
-#pragma once
-#include "GameObject.h"
-#include "snake.h"
+#pragma once //prevents file from being included twice 
+#include "GameObject.h" //base class - will inherit draw() and update()
+#include "snake.h" //for position
 #include <cstdlib>
 
-enum FoodType { NORMAL, SPECIAL };
+enum FoodType { NORMAL, SPECIAL }; //normal 0 special 1 
 
 class Food : public GameObject {
 public:
     Food(int boardRows, int boardCols);
 
+    //inherited
     void draw() override;
     void update() override;
 
+    //new (5) - pos, type, points, respawn, symbol
     Position getPosition() const;
     FoodType getType() const;
     int getPoints() const;
@@ -23,7 +25,7 @@ public:
 private:
     Position pos;
     FoodType type;
-    int boardRows, boardCols;
+    int boardRows, boardCols; //when randomize picks a new position it needs to know boundaries 
     int specialTimer;
 
     void randomize();
