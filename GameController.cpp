1a5819c5a6ac06593a45218f00b7a7d0b848fec2
nehacharyani <nehacharyani123@gmail.com>
2026-05-11 23:17:14 +0500

    neha added comments

--- GameController.cpp
+++ GameController.cpp
@@ -2,7 +2,7 @@
 #include "GameController.h"
 #include "raylib.h"
 #include <cstdlib>
-
+//constructor of gameController class
 GameController::GameController()
     : food(26, 26),
     // x=5 and y=13 for snake1 starting point
@@ -41,9 +41,10 @@ void GameController::resetGame() {
 }
 
 //AI Logic 
+//returns Direction-enum class in snake.h
 Direction GameController::getAIDirection() {
-    Position head = snake2.getHead();
-    Position food = this->food.getPosition();
+    Position head = snake2.getHead();//getting snakes direction
+    Position food = this->food.getPosition();//foods direction
 
     Direction preferred[4];
     int count = 0;
@@ -75,10 +76,10 @@ Direction GameController::getAIDirection() {
 
         bool safe = true;
         if (next.x < 0 || next.x >= 26 || next.y < 0 || next.y >= 26) safe = false;
-        if (snake2.containsPosition(next)) safe = false;
+        if (snake2.containsPosition(next)) safe = false;//self hit
         if (snake1.containsPosition(next)) safe = false;
         for (int j = 0; j < (int)obstacles.size(); j++)
-        if (next == obstacles[j].getPosition()) safe = false;
+        if (next == obstacles[j].getPosition()) safe = false;//hitting object
 
         if (safe) return d;
     }
